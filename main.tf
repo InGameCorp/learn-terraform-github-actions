cddeea68a0c47f6b09394ed2d21b1209416582ad
Lucas Vásquez <lgabrielvleon@gmail.com>
2023-11-04 14:10:14 -0300

    Update VPC

--- main.tf
+++ main.tf
@@ -10,14 +10,20 @@ terraform {
   }
 }
 
+locals {
+  environment = "Sandbox"
+  vpc_name = "vpccue1ingamesbx001"
+}
+
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "example" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "vpccue1ingamesbx001"
+    Name = local.vpc_name
+    Environment = local.environment
   }
 }
 
